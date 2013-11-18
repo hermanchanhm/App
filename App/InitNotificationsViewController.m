74272dbd1321f6555cf9f31855bb5bfeb60f27b2
hermanchanhm <hermanchanhm@gmail.com>
2013-11-18 04:05:46 -0800

    End of today’s coding. Notifications successfully implemented

---
+++ App/InitNotificationsViewController.m
@@ -0,0 +1,71 @@
+//
+//  InitNotificationsViewController.m
+//  App
+//
+//  Created by Herman on 11/18/13.
+//  Copyright (c) 2013 Team 22. All rights reserved.
+//
+
+#import "InitNotificationsViewController.h"
+
+@interface InitNotificationsViewController ()
+
+@end
+
+@implementation InitNotificationsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)nextTapped:(id)sender {
+    
+    //AM Notification
+    NSDate *AMPickerDate = [self.AMPicker date];
+    
+    UILocalNotification* AMNotification = [[UILocalNotification alloc] init];
+    AMNotification.fireDate = AMPickerDate;
+    AMNotification.alertBody = @"How Motivated Are You? Fill out the survey for today!";
+    AMNotification.alertAction = @"Good Morning!";
+    AMNotification.applicationIconBadgeNumber = [[UIApplication sharedApplication] applicationIconBadgeNumber] + 1;
+    AMNotification.timeZone = [NSTimeZone defaultTimeZone];
+    
+     [[UIApplication sharedApplication] scheduleLocalNotification:AMNotification];
+    //Repeat Daily
+    AMNotification.repeatInterval = NSDayCalendarUnit;
+    
+    //End AM Notification
+    
+    //PM Notification
+    NSDate *PMPickerDate = [self.PMPicker date];
+    
+    UILocalNotification* PMNotification = [[UILocalNotification alloc] init];
+    PMNotification.fireDate = PMPickerDate;
+    PMNotification.alertBody = @"How many Goals you have achieved? Take the survey now!";
+    PMNotification.alertAction = @"Good Evening!";
+    PMNotification.applicationIconBadgeNumber = [[UIApplication sharedApplication] applicationIconBadgeNumber] + 1;
+    PMNotification.timeZone = [NSTimeZone defaultTimeZone];
+    
+    [[UIApplication sharedApplication] scheduleLocalNotification:PMNotification];
+    //Repeat Daily
+    PMNotification.repeatInterval = NSDayCalendarUnit;
+    
+}
+@end
--- App/MainAppDelegate.m
+++ App/MainAppDelegate.m
@@ -13,6 +13,7 @@
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
     // Override point for customization after application launch.
+    
     return YES;
 }
 							
@@ -41,6 +42,14 @@
 - (void)applicationWillTerminate:(UIApplication *)application
 {
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+}
+
+
+//**self-implemented**Set what the system will do after notification alert clicked when App is in foregroud, change badge to 0
+- (void) application:(UIApplication *)application didReceiveLocalNotification:(UILocalNotification *)notification
+{
+
+        application.applicationIconBadgeNumber = 0;
 }
 
 @end
